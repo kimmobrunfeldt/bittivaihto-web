5afa6bcde101ec80ef789624a48a11df3d1bd0e0
Vesa Uimonen <vesa.uimonen@gmail.com>
2013-09-07 00:17:44 +0300

    Overrided SQLAlchemy Column with sqlalchemy_defaults Column

--- socialpainting/extensions.py
+++ socialpainting/extensions.py
@@ -1,6 +1,11 @@
+import sqlalchemy as sa
 from flask.ext.sqlalchemy import SQLAlchemy
 from raven.contrib.flask import Sentry
+from sqlalchemy_defaults import Column, make_lazy_configured
 
 
 db = SQLAlchemy()
 sentry = Sentry(logging=True)
+
+db.Column = Column
+make_lazy_configured(sa.orm.mapper)
