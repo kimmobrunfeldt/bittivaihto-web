0d37a802d27a1421e50b48b8cc7a13f558f88898
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-01-05 18:06:45 +0200

    Try to fix

--- bittivaihto/wsgi.py
+++ bittivaihto/wsgi.py
@@ -3,9 +3,11 @@ import newrelic.agent
 from . import Application
 
 app = Application()
-app = newrelic.agent.wsgi_application()(app.wsgi_app)
 
 @app.route("/google78fada8781858043.html")
 def site_verification():
     """Returns site verification content"""
     return 'google-site-verification: google78fada8781858043.html'
+
+
+app = newrelic.agent.wsgi_application()(app.wsgi_app)
