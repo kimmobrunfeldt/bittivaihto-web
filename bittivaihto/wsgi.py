ca19cb599d37065c7601d9f31273ce89a44b3984
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-01-05 17:57:39 +0200

    Add google site verification very stupidly to wsgi file

--- bittivaihto/wsgi.py
+++ bittivaihto/wsgi.py
@@ -4,3 +4,8 @@ from . import Application
 
 app = Application()
 app = newrelic.agent.wsgi_application()(app.wsgi_app)
+
+@app.route("/google78fada8781858043.html")
+def site_verification():
+    """Returns site verification content"""
+    return 'google-site-verification: google78fada8781858043.html'
